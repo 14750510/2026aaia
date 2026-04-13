5df85fcf9f1f41aa4711e1c73ab72113a87e6f57
14750510 <14750510@me.mcu.edu.tw>
2026-04-13 12:03:36 +0800

    Add files via upload

---
+++ week08/week08-5a.cpp
@@ -0,0 +1,9 @@
+//week08-5a.cpp SOIT107_Base_009
+#include <stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	if(n>2)printf("%d",n-2);
+	else printf("%d",2-n);
+}
---
+++ week08/week08-5c.cpp
@@ -0,0 +1,8 @@
+//week08-5c.cpp SOIT107_Base_013
+#include <stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	printf("%d",(n-1)%7);
+}
